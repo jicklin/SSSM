5621984effd6e72e011e83a4918f0c2747cc62b8
JackLin <boyfighting@qq.com>
2017-04-04 11:59:27 +0800

    Revert "Revert "测试jstl标签""
    This reverts commit 79af97976afe62c2363a647948776441c94a12a4.

--- src/main/java/com/jiajia/controller/SearchController.java
+++ src/main/java/com/jiajia/controller/SearchController.java
@@ -2,12 +2,13 @@ package com.jiajia.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.jiajia.entity.UserEntity;
-import com.sun.corba.se.spi.orbutil.fsm.Guard;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -19,15 +20,27 @@ public class SearchController {
 
     @RequestMapping("/family")
     @ResponseBody
-    public String testSearch(@RequestBody List<UserEntity> users) {
+    public String testSearch(@RequestBody List<UserEntity> users, Model model) {
         String result = "";
         if (users == null || users.size() == 0) {
             return "没有记录";
         }
+        model.addAttribute("users",users);
         result = JSON.toJSONString(users);
         System.out.println(result);
         return result;
 
+
+    }
+    @RequestMapping("/family2")
+    public String  testJstl(Model model){
+        List<UserEntity> users=new ArrayList<UserEntity>();
+        UserEntity user1=new UserEntity();
+        user1.setUsername("马林");
+        user1.setCertNo("123");
+        users.add(user1);
+        model.addAttribute("user2",users);
+        return "success";
 
     }
 
